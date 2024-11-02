8105b6d041b06d695aae516b24f8a09dfee1c821
Pipegil <pipex700@gmail.com>
2024-11-02 17:46:31 -0500

    Video 001 finished

--- components/backoffice/WeeklySalesChart.jsx
+++ components/backoffice/WeeklySalesChart.jsx
@@ -1,18 +1,84 @@
 "use client";
+import { LineElement } from "chart.js";
 import React, { useState } from "react";
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
 export default function WeeklySalesChart() {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Weekly Sales",
+      },
+    },
+  };
+  const labels = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
   const tabs = [
     {
       title: "Sales",
       type: "sales",
+      data: {
+        labels,
+        datasets: [
+          {
+            label: "Sales",
+            data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+            borderColor: "rgb(22, 101, 52)",
+            backgroundColor: "rgb(22, 163, 74)",
+          },
+        ],
+      },
     },
     {
       title: "Orders",
       type: "orders",
+      data: {
+        labels,
+        datasets: [
+          {
+            label: "Orders",
+            data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+            borderColor: "rgb(30, 64, 175)",
+            backgroundColor: "rgb(37, 99, 235)",
+          },
+        ],
+      },
     },
   ];
-  const [chartToBeDisplayed, setChartToBeDisplayed] = useState("sales");
+  const [chartToBeDisplayed, setChartToBeDisplayed] = useState(tabs[0].type);
   return (
     <div className="bg-slate-700 p-8 rounded-lg">
       <h2 className="text-xl font-bold mb-4">Weekly Sales</h2>
@@ -20,21 +86,30 @@ export default function WeeklySalesChart() {
         {/* Tabs */}
         <div className="text-sm font-medium text-center text-gray-200 border-b border-gray-400 dark:text-gray-400 dark:border-gray-700">
           <ul className="flex flex-wrap -mb-px">
-            <li className="me-2">
-              <button className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-300 hover:border-gray-100 dark:hover:text-gray-100">
-                Sales
-              </button>
-            </li>
-            <li className="me-2">
-              <button
-                className="inline-block p-4 text-orange-600 border-b-2 border-orange-600 rounded-t-lg active dark:text-orange-500 dark:border-orange-500"
-                aria-current="page">
-                Orders
-              </button>
-            </li>
+            {tabs.map((tabs, index) => {
+              return (
+                <li className="me-2" key={index}>
+                  <button
+                    onClick={() => setChartToBeDisplayed(tabs.type)}
+                    className={
+                      chartToBeDisplayed == tabs.type
+                        ? "inline-block p-4 text-orange-600 border-b-2 border-orange-600 rounded-t-lg active dark:text-orange-500 dark:border-orange-500"
+                        : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-300 hover:border-gray-100 dark:hover:text-gray-100"
+                    }>
+                    {tabs.title}
+                  </button>
+                </li>
+              );
+            })}
           </ul>
         </div>
         {/* Content to be displayed */}
+        {tabs.map((tab, index) => {
+          if (chartToBeDisplayed === tab.type) {
+            return <Line options={options} data={tab.data} />;
+          }
+          return null;
+        })}
       </div>
     </div>
   );
